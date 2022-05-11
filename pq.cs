114b2d094a05efeda09f02efdbca0211b6dfc160
Riley Schneider <88947587+Ferryry@users.noreply.github.com>
2022-05-12 00:35:20 +0200

    pq.cs

---
+++ pq.cs
@@ -0,0 +1,9 @@
+public static double[] pq(double p, double q)
+{
+  if(Math.Pow(p/2, 2) - q >= 0)
+    return new double[] {
+      -p/2 - Math.Sqrt(Math.Pow(p/2, 2) - q ),
+      p/2 + Math.Sqrt(Math.Pow(p/2, 2) - q )
+    };
+   else throw new Exception("Keine Nullstelle!");
+}
