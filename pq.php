fdac3bb123cdbb651578444d298a2ffd89f71266
Riley Schneider <88947587+Ferryry@users.noreply.github.com>
2022-05-12 00:22:16 +0200

    pq.php

---
+++ pq.php
@@ -0,0 +1,9 @@
+<?php
+function pq($p, $q)
+{
+  if(pow($p/2, 2) - $q >= 0)
+    return  array(-$p/2 - sqrt(pow($p/2, 2) - $q), -$p/2 + sqrt(pow($p/2, 2) - $q) );
+  else
+    die("Keine Nullstelle");
+}
+?>
