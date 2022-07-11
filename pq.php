abf84e93eeb46a9451958c5baec591792e95e96b
Riley Schneider <88947587+Ferryry@users.noreply.github.com>
2022-07-11 20:07:06 +0200

    Update pq.php

--- pq.php
+++ pq.php
@@ -1,8 +1,8 @@
 <?php
-function pq($p, $q)
+function pq($p, $q): array
 {
   if(pow($p/2, 2) - $q >= 0)
-    return  array(-$p/2 - sqrt(pow($p/2, 2) - $q), -$p/2 + sqrt(pow($p/2, 2) - $q) );
+    return array(-$p/2 - sqrt(pow($p/2, 2) - $q), -$p/2 + sqrt(pow($p/2, 2) - $q) );
   else
     die("Keine Nullstelle");
 }
